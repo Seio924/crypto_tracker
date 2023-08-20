221e37ae059e750475343d1685d4bd677df95792
Seio924 <134473627+Seio924@users.noreply.github.com>
2023-08-20 14:09:35 +0900

    coinlist

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,50 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { styled } from "styled-components";
+import { useEffect, useState } from "react";
 
-interface Routeparams{
-    coinId: string;
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface Routeparams {
+  coinId: string;
 }
 
-function Coin(){
-    const { coinId } = useParams<Routeparams>();
-    return <h1>Coin: {coinId}</h1>;
+interface RouteState {
+  state: string;
+}
+
+function Coin() {
+  const { coinId } = useParams<Routeparams>();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
